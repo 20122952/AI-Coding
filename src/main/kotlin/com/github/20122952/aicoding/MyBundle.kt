2044a79daaf6373ed05879f24e383d1d03730537
GitHub Action <action@github.com>
2025-09-10 15:40:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/20122952/aicoding/MyBundle.kt
+++ src/main/kotlin/com/github/20122952/aicoding/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.20122952.aicoding
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/20122952/aicoding/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/20122952/aicoding/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.20122952.aicoding.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
